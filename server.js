431a3e7da4e9a88c81298382ae5d3426cae97ed3
Evan Freymiller <ejf1990@gmail.com>
2020-08-10 22:41:31 -0500

    server fixes

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.set('x-powered-by', false);
   // Handle React routing, return all requests to React app
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "/build", "index.html"));
   });
 }
 
