e87de9539416b33f6739c63077fd4525288cd00f
Evan Freymiller <ejf1990@gmail.com>
2020-08-10 22:18:48 -0500

    fixed hot button

--- src/components/ThemeToggle/index.jsx
+++ src/components/ThemeToggle/index.jsx
@@ -9,22 +9,23 @@ const ThemeToggle = () => {
     const { dispatch, isDay } = React.useContext(Context);
 
     return (
-        <Flex justifyContent="flex-end" alignItems="flex-end"  margin="10px" className="show-zoom-animation">
-            <FontAwesomeIcon
-                size="lg"
-                color={isDay ? 'white' : COLOR}
-                icon={isDay ? faFireExtinguisher : faFire}
-                onClick={() => {
-                    isDay ? dispatch({ type: "SET_IS_NIGHT" }) : dispatch({ type: "SET_IS_DAY" });
-                }}
-                style={{
-                    padding: "10px 17px 10px 17px",
-                    backgroundColor: isDay ? COLOR : COLOR_BRIGHT,
-                    borderRadius: 100,
-                }}
-                className="navigation-button"
-            />
-        </Flex>
+        <FontAwesomeIcon
+            size="lg"
+            color={isDay ? 'white' : COLOR}
+            icon={isDay ? faFireExtinguisher : faFire}
+            onClick={() => {
+                isDay ? dispatch({ type: "SET_IS_NIGHT" }) : dispatch({ type: "SET_IS_DAY" });
+            }}
+            style={{
+                padding: "10px 17px 10px 17px",
+                backgroundColor: isDay ? COLOR : COLOR_BRIGHT,
+                borderRadius: 100,
+                position: 'absolute',
+                bottom: '50px',
+                right: '50px',
+            }}
+            className="navigation-button show-zoom-animation"
+        />
     )
 }
 
